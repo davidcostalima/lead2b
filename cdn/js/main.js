822006b8d66b05d8f877ca6865305e50145ac576
brunocriacoes <br.rafael@outlook.com>
2022-04-21 11:28:08 -0300

    fix

--- cdn/js/main.js
+++ cdn/js/main.js
@@ -16,7 +16,8 @@ Vue.createApp({
                     title: "Eu preciso de um box no tamanho:",
                     fields: [],
                     radioName: "tamanho",
-                    radio: [                        
+                    radioValue: null,
+                    radio: [
                         { label: "PP", value: "0" },
                         { label: "P", value: "1" },
                         { label: "M", value: "2" },
@@ -28,7 +29,7 @@ Vue.createApp({
                     title: "Para onde devemos enviar o orçamento instantâneo com promoção imperdível?",
                     fields: [
                         { type: "email", title: "E-mail", value: null },
-                        { type: "text",  title: "Telefone", value: null },                        
+                        { type: "text", title: "Telefone", value: null },
                     ],
                     radioName: null,
                     radio: []
@@ -37,12 +38,14 @@ Vue.createApp({
                     title: "Metragens",
                     fields: [],
                     radioName: "metragem",
+                    radioValue: null,
                     radio: []
                 },
                 {
                     title: "Tempo de Locação",
                     fields: [],
                     radioName: "tempo",
+                    radioValue: null,
                     radio: [
                         { label: "1 mês", value: "1" },
                         { label: "2 a 3 meses", value: "3" },
@@ -55,6 +58,7 @@ Vue.createApp({
                     title: "Olá, João. O seu orçamento é",
                     fields: [],
                     radioName: "orcamento",
+                    radioValue: null,
                     radio: [
                         { label: "R$XX a R$XX por mês (você sabia que 80% das pessoas escolhe um box maior…) Para um orçamento mais preciso: ", value: "1" },
                         { label: "Quero falar com uma pessoa agora mesmo", value: "3" },
@@ -73,6 +77,7 @@ Vue.createApp({
                     title: "",
                     fields: [],
                     radioName: "guardar",
+                    radioValue: null,
                     radio: [
                         { label: "Eu vou guardar itens da minha empresa", value: "3" },
                         { label: "Eu vou guardar itens da minha casa", value: "6" },
@@ -80,19 +85,74 @@ Vue.createApp({
                 },
                 {
                     title: "Eu vou guardar itens da sala",
-                    fields: [],
+                    fields: [
+                        { type: 'number', title: "Aparador", value: 0, size: 0.33 },
+                        { type: 'number', title: "Banqueta - Puff", value: 0, size: 0.11 },
+                        { type: 'number', title: "Bar grande", value: 0, size: 1.1 },
+                        { type: 'number', title: "Bar pequeno", value: 0, size: 0.66 },
+                        { type: 'number', title: "Baú", value: 0, size: 0.44 },
+                        { type: 'number', title: "Buffet", value: 0, size: 1.1 },
+                        { type: 'number', title: "Cadeira com braços", size: 0.33, value: 0 },
+                        { type: 'number', title: "Cadeira simples", size: 0.22, value: 0 },
+                        { type: 'number', title: "Console de parede", size: 0.33, value: 0 },
+                        { type: 'number', title: "Cristaleira", size: 0.88, value: 0 },
+                        { type: 'number', title: "Escrivaninha grande", size: 1.1, value: 0 },
+                        { type: 'number', title: "Escrivaninha pequena", size: 0.66, value: 0 },
+                        { type: 'number', title: "Estante alta", size: 0.66, value: 0 },
+                        { type: 'number', title: "Estante baixa", size: 0.33, value: 0 },
+                        { type: 'number', title: "Mesa de centro", size: 0.33, value: 0 },
+                        { type: 'number', title: "Mesa console", size: 0.33, value: 0 },
+                        { type: 'number', title: "Mesa de jantar", size: 0.88, value: 0 },
+                        { type: 'number', title: "Piano armário", size: 6.6, value: 0 },
+                        { type: 'number', title: "Piano de cauda", size: 6.6, value: 0 },
+                        { type: 'number', title: "Poltrona simples", size: 0.55, value: 0 },
+                        { type: 'number', title: "Quadros", size: 0.11, value: 0 },
+                        { type: 'number', title: "Rack", size: 0.33, value: 0 },
+                        { type: 'number', title: "Sofá 2 lugares", size: 1.1, value: 0 },
+                        { type: 'number', title: "Sofá 3 lugares", size: 1.65, value: 0 },
+                        { type: 'number', title: "Sofá 4 lugares", size: 2.2, value: 0 },
+                        { type: 'number', title: "Som/Vídeo", size: 0.33, value: 0 },
+                        { type: 'number', title: "TV", size: 0.33, value: 0 },
+                    ],
                     radioName: "",
                     radio: []
                 },
                 {
                     title: "Eu vou guardar itens da cozinha",
-                    fields: [],
+                    fields: [
+                        { type: "number", title: "Armário paneleiro", size: 1.1, value: 0 },
+                        { type: "number", title: "Armário de parede pequeno com 2 portas", size: 0.22, value: 0 },
+                        { type: "number", title: "Cadeira", size: 0.66, value: 0 },
+                        { type: "number", title: "Exaustor", size: 0.11, value: 0 },
+                        { type: "number", title: "Freezer", size: 0.88, value: 0 },
+                        { type: "number", title: "Fogão (4 bocas)", size: 0.33, value: 0 },
+                        { type: "number", title: "Fogão (6 bocas)", size: 0.55, value: 0 },
+                        { type: "number", title: "Forninho", size: 0.11, value: 0 },
+                        { type: "number", title: "Geladeira", size: 0.99, value: 0 },
+                        { type: "number", title: "Máquina de lavar louça", size: 0.55, value: 0 },
+                        { type: "number", title: "Máquina de lavar/secar", size: 0.55, value: 0 },
+                        { type: "number", title: "Mesa de cozinha", size: 0.33, value: 0 },
+                        { type: "number", title: "Micro-ondas", size: 0.11, value: 0 },
+                    ],
                     radioName: "",
                     radio: []
                 },
                 {
                     title: "Eu vou guardar itens do quarto",
-                    fields: [],
+                    fields: [
+                        { type: "number", title: "Arca", size: 0.44, value: 0 },
+                        { type: "number", title: "Armário 2 portas desmontado", size: 1.1, value: 0 },
+                        { type: "number", title: "Armário 3 portas desmontado", size: 1.65, value: 0 },
+                        { type: "number", title: "Berço com colchão", size: 0.33, value: 0 },
+                        { type: "number", title: "Cabideiro", size: 0.33, value: 0 },
+                        { type: "number", title: "Cama beliche com colchão", size: 2.2, value: 0 },
+                        { type: "number", title: "Cama casal com colchão", size: 1.43, value: 0 },
+                        { type: "number", title: "Cama de solteiro com colchão", size: 1.1, value: 0 },
+                        { type: "number", title: "Cômoda", size: 0.44, value: 0 },
+                        { type: "number", title: "Mesa de cabeceira", size: 0.22, value: 0 },
+                        { type: "number", title: "Penteadeira", size: 0.77, value: 0 },
+                        { type: "number", title: "Quadros", size: 0.11, value: 0 },
+                    ],
                     radioName: "",
                     radio: []
                 },
@@ -100,13 +160,21 @@ Vue.createApp({
                     title: "Eu gostaria que entrem em contato por",
                     fields: [],
                     radioName: "",
+                    radioValue: null,
                     radio: [
                         { label: "WhatsApp", value: "6" },
                         { label: "Email", value: "6" },
                         { label: "Telefone", value: "6" },
                     ]
                 },
-                          
+                {
+                    title: "Orçamento solicitado com sucesso",
+                    fields: [],
+                    radioName: "",
+                    radioValue: null,
+                    radio: []
+                },
+
             ]
         }
     },
@@ -117,6 +185,7 @@ Vue.createApp({
         back() {
             this.step--
             this.save()
+            this.handleMetros()
         },
         finish() {
             localStorage.removeItem('form_temp')
@@ -124,11 +193,38 @@ Vue.createApp({
         },
         next() {
             this.step++
-            this.onNext=true
+            this.onNext = true
             this.save()
+            this.handleMetros()
         },
         isNext() {
-            this.onNext=false
+            this.onNext = false
+        },
+        handleMetros() {
+            let tipo = this.steps[0].value
+            this.steps[2].radio = []
+            for (let i = 1; i < 32; i++) {
+                this.steps[2].radio.push({ label: `${i}m²`, value: i })
+            }
+
+            switch (tipo) {
+                case 'PP':
+                    this.steps[2].radio = Array.from(this.steps[2].radio).filter(e => e.value > 0 && e.value < 4)
+                    break;
+                case 'P':
+                    this.steps[2].radio = Array.from(this.steps[2].radio).filter(e => e.value > 3 && e.value < 7)
+                    break;
+                case 'M':
+                    this.steps[2].radio = Array.from(this.steps[2].radio).filter(e => e.value > 6 && e.value < 10)
+                    break;
+                case 'G':
+                    this.steps[2].radio = Array.from(this.steps[2].radio).filter(e => e.value > 9 && e.value < 32)
+                    break;
+                case 'SOB':
+                    this.steps[2].radio = Array.from(this.steps[2].radio).filter(e => e.value > 0 && e.value < 0)
+                    break;
+
+            }
         },
         save() {
             return null
@@ -141,24 +237,24 @@ Vue.createApp({
                 total: this.total,
                 steps: this.steps
             }))
+
         },
         handleCalc() {
-            let total = this.steps.reduce( (acc, e) => {
-                acc = [...acc,...e.fields.map( f => f.quant * f.size)]
+            let total = this.steps.reduce((acc, e) => {
+                acc = [...acc, ...e.fields.map(f => f.quant * f.size)]
                 return acc
-            }, [] )
-            this.total = total.reduce( (acc, v) =>  acc + v, 0 )
+            }, [])
+            this.total = total.reduce((acc, v) => acc + v, 0)
         }
     },
-    mounted() {        
-        // let form_temp = JSON.parse(localStorage.getItem('form_temp'))
-        // if (form_temp) {
-        //     Object.keys(form_temp).forEach(key => {
-        //         this[key] = form_temp[key]
-        //     })
-        // }
-        for( let i = 1; i < 32; i++) {
-            this.steps[2].radio.push( { label: `${i}m²`, value: "0" } )
+    mounted() {
+        let form_temp = JSON.parse(localStorage.getItem('form_temp'))
+        if (form_temp) {
+            // Object.keys(form_temp).forEach(key => {
+            //     this[key] = form_temp[key]
+            // })
         }
+
+        this.handleMetros()
     }
 }).mount('#js-app')
