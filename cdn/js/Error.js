a4027afa34ac1554826c7b81efa872a306ce1eed
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-10-06 13:58:33 -0300

    fix

---
+++ cdn/js/Error.js
@@ -0,0 +1,9 @@
+export default class ErrorDefault{
+    error_default(){
+        return {
+            status_code: 500,
+            next: false,
+            message: 'Não foi possível acessar api'
+        }
+    }
+}
