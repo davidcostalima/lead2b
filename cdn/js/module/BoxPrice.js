239414d02a995b591e3b6e70052a6dc99e9f3faf
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-10-06 22:08:34 -0300

    fix

--- cdn/js/module/BoxPrice.js
+++ cdn/js/module/BoxPrice.js
@@ -1,3 +1,25 @@
-export default BoxPrice {
-    
+export default class BoxPrice {
+    constructor() {
+        this.lb = {
+            pp: {
+                primaryPrice: "R$37,00",
+                secondPrice: "R$70,00"
+            },
+            p: {
+                primaryPrice: "R$137,00",
+                secondPrice: "R$170,0"
+            },
+            m: {
+                primaryPrice: "R$237,00",
+                secondPrice: "R$70,00"
+            },
+            g: {
+                primaryPrice: "R$337,00",
+                secondPrice: "R$370,00"
+            }
+        }
+    }
+    getPtrice(typeSize) {
+       return this.lb[typeSize] || this.lb.pp 
+    }
 }
